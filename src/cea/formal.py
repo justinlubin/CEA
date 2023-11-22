cd71f7da7a6c1fc227f3436eed0681315aa7bda7
Justin Lubin <jlubi333@gmail.com>
2023-11-21 17:49:43 -0800

    More work on figuring out design

--- src/cea/formal.py
+++ src/cea/formal.py
@@ -1,77 +1,304 @@
-from typing import Callable, Self
 from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass
+from typing import Sequence, Callable, Self
+import inspect
 
-Sort = str
-Symbol = str
+
+# https://stackoverflow.com/a/952952
+def flatten(l):
+    return [item for sublist in l for item in sublist]
 
 
-class Signature:
-    sorts: set[Sort]
-    funs: set[Symbol]
-    rels: set[Symbol]
-    ar: dict[Symbol, list[Sort]]
+################################################################################
+# Core
 
-    def __init__(
-        self,
-        sorts: set[Sort],
-        funs: set[Symbol],
-        rels: set[Symbol],
-        ar: dict[Symbol, list[Sort]],
-    ):
-        if funs & rels:
-            raise ValueError("Overlapping function and relation symbols")
 
-        if ar.keys() != (funs | rels):
-            raise ValueError("Invalid arity domain")
+class Var(metaclass=ABCMeta):
+    name: str
 
-        for a in ar:
-            if not (set(ar[a]) <= sorts):
-                raise ValueError(f"Invalid arity codomain for {a}")
+    def __str__(self) -> str:
+        return self.name
 
-        self.sorts = sorts
-        self.funs = funs
-        self.rels = rels
-        self.ar = ar
 
-    def compatible(self, other: "Signature") -> bool:
+class Term(metaclass=ABCMeta):
+    @classmethod
+    @abstractmethod
+    def var(cls, name: str) -> Var:
+        pass
+
+
+@dataclass
+class Relation:
+    name: str
+    arity: Sequence[type[Term]]
+
+
+class Atom:
+    rel: Relation
+    args: Sequence[Term]
+
+    def __init__(self, rel: Relation, args: Sequence) -> None:
+        if len(rel.arity) != len(args):
+            raise ValueError("Mismatched arity length")
+        for i, (s, arg) in enumerate(zip(rel.arity, args)):
+            if not isinstance(arg, s):
+                raise ValueError(f"Mismatched arity: arg {i}")
+
+        self.rel = rel
+        self.args = args
+
+    def __str__(self) -> str:
+        return str(self.rel.name) + "(" + ", ".join(map(str, self.args)) + ")"
+
+
+@dataclass
+class Formula:
+    fvs: Sequence[Sequence[Var]]
+    conds: Sequence[Atom]
+
+
+@dataclass
+class Function:
+    name: str
+    dom: Sequence[type]
+    cod: type
+    precondition: Formula
+
+
+@dataclass
+class Rule:
+    name: str
+    head: Atom
+    body: Sequence[Atom]
+
+    def __str__(self) -> str:
         return (
-            self.sorts == other.sorts
-            and not (self.funs & other.funs)
-            and not (self.rels & other.rels)
+            f"== {self.name} ============================\n"
+            + "\n".join(map(str, self.body))
+            + "\n--------------------\n"
+            + str(self.head)
+            + "\n=============================="
+            + "=" * (len(self.name) + 2)
         )
 
-    def union(self, other: "Signature") -> "Signature":
-        if not self.compatible(other):
-            raise ValueError("Cannot union incompatible signatures")
 
-        return Signature(
-            self.sorts,
-            self.funs | other.funs,
-            self.rels | other.rels,
-            self.ar | other.ar,
-        )
+################################################################################
+# Framework
 
-    def relational(self) -> bool:
-        return not self.funs
-
-    def functional(self) -> bool:
-        return not self.rels
+RELATIONS: dict[type, Relation] = {}
 
 
-value_types = Signature(
-    sorts={"Time"},
-    funs=set(),
-    rels={"TimeEq", "TimeLt"},
-    ar={"TimeEq": ["Time", "Time"], "TimeLt": ["Time", "Time"]},
+def primitive(*arity: type):
+    def decorator(cls: type) -> type:
+        RELATIONS[cls] = Relation(cls.__name__, arity)
+        return cls
+
+    return decorator
+
+
+def event(*arity: type):
+    def decorator(cls: type) -> type:
+        RELATIONS[cls] = Relation(cls.__name__, arity)
+        return cls
+
+    return decorator
+
+
+def analysis(*arity: type):
+    def decorator(cls: type) -> type:
+        RELATIONS[cls] = Relation(cls.__name__, arity)
+        return cls
+
+    return decorator
+
+
+API: list[Function] = []
+
+
+def precondition(pc: Callable):
+    def wrapper(func: Callable) -> Callable:
+        sig = inspect.signature(func)
+        var_names = list(inspect.signature(pc).parameters.keys())
+
+        name = func.__name__
+        cod = sig.return_annotation
+
+        dom = []
+        fvs: list[list[Var]] = []
+        i = 0
+        for p in sig.parameters.values():
+            dom.append(p.annotation)
+            fvs.append([])
+            for a in RELATIONS[p.annotation].arity:
+                fvs[-1].append(a.var(var_names[i]))
+                i += 1
+
+        fvs.append([])
+        for a in RELATIONS[cod].arity:
+            fvs[-1].append(a.var(var_names[i]))
+            i += 1
+
+        conds = pc(*flatten(fvs))
+
+        API.append(Function(name, dom, cod, Formula(fvs, conds)))
+
+        return func
+
+    return wrapper
+
+
+def rule_of_fun(f: Function) -> Rule:
+    return Rule(
+        f.name,
+        Atom(RELATIONS[f.cod], f.precondition.fvs[-1]),
+        [Atom(RELATIONS[t], fv) for t, fv in zip(f.dom, f.precondition.fvs[:-1])]
+        + list(f.precondition.conds),
+    )
+
+
+################################################################################
+# Primitives
+
+### Time
+
+
+# Terms
+
+
+class Time(Term, metaclass=ABCMeta):
+    def __eq__(self, other) -> Atom:  # type: ignore[override]
+        return Atom(RELATIONS[TimeEq], [self, other])
+
+    def __lt__(self, other) -> Atom:  # type: ignore[override]
+        return Atom(RELATIONS[TimeLt], [self, other])
+
+    def uniq(self) -> Atom:
+        return Atom(RELATIONS[TimeUnique], [self])
+
+    @classmethod
+    def var(cls, name: str) -> Var:
+        return TimeVar(name)
+
+
+class TimeVar(Time, Var):
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
+
+
+class TimeLit(Time):
+    day: int
+
+    def __init__(self, day: int):
+        if day < 0:
+            raise ValueError("Negative day")
+        self.day = day
+
+
+# Relations
+
+
+@primitive(Time, Time)
+class TimeEq:
+    pass
+
+
+@primitive(Time, Time)
+class TimeLt:
+    pass
+
+
+@primitive(Time)
+class TimeUnique:
+    pass
+
+
+### Conditions
+
+# Terms
+
+
+class Cond(Term, metaclass=ABCMeta):
+    def __eq__(self, other) -> Atom:  # type: ignore[override]
+        return Atom(RELATIONS[CondEq], [self, other])
+
+    @classmethod
+    def var(cls, name: str) -> Var:
+        return CondVar(name)
+
+
+class CondVar(Cond, Var):
+    name: str
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+
+class CondLit(Cond):
+    cond: str
+
+    def __init__(self, cond: str) -> None:
+        self.cond = cond
+
+
+# Relations
+
+
+@primitive(Cond, Cond)
+class CondEq:
+    pass
+
+
+################################################################################
+# Instantiation
+
+### Events
+
+
+@event(Time, Cond)
+@dataclass
+class Transfect:
+    library: str
+
+
+@event(Time, Cond)
+@dataclass
+class Seq:
+    results: str
+
+
+### Analyses
+
+
+@analysis(Time, Time, Cond)
+@dataclass
+class SGRE:
+    fold_change: float
+    sig: float
+
+
+### API
+
+
+@precondition(
+    # lambda tr, s1, s2, ret:
+    lambda t0, c0, t1, c1, t2, c2, t3a, t3b, c3: [
+        t0 < t1,
+        t1 < t2,
+        t0.uniq(),
+        t1 == t3a,
+        t2 == t3b,
+        c0 == c1,
+        c0 == c2,
+        c0 == c3,
+    ],
 )
+def sgre(tr: Transfect, s1: Seq, s2: Seq) -> SGRE:
+    return SGRE(2, 0.03)
 
-event_types = Signature(
-    sorts=value_types.sorts,
-    funs=set(),
-    rels={"Seq", "Transfect"},
-    ar={"Seq": ["Time"], "Transfect": ["Time"]},
-)
 
-assert event_types.relational()
-assert event_types.compatible(value_types)
+################################################################################
+# Scratch
+
+print(rule_of_fun(API[0]))
